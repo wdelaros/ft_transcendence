c3bcd6acec27a3d29fef29e933d1fa8f3bd756d1
wdelaros <williamdelarosbil@gmail.com>
2025-01-10 14:28:01 -0500

    le commencement de la vrai team Original Gros

---
+++ buildTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sleep 10
+running="$(docker-compose -f "./srcs/docker-compose-test.yml" -p transcendence-test ps --services --filter "status=running")"
+services="$(docker-compose -f "./srcs/docker-compose-test.yml" -p transcendence-test ps --services)"
+if [ "$running" != "$services" ]; then
+    echo "Following services are not running:" 
+    # Bash specific
+    comm -13 <(sort <<<"$running") <(sort <<<"$services")
+    exit 1
+else
+    echo "All services are running"
+    exit 0
+fi
